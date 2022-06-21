a1921c38ff0cc6125663850c3bb9a89b527d2d61
Raquel Teixeira <raquelat@ciandt.com>
2022-06-21 15:58:00 -0300

    db

---
+++ 0x00/salvando/sql_schema_first_db.sql
@@ -0,0 +1,10 @@
+-- TABLE
+CREATE TABLE CLIENTE (id int primary key not null, nome varchar(80) not null, email varchar(40) not null, telefone int);
+CREATE TABLE CLIENTES (id int primary key not null, nome varchar(80) not null, email varchar(40) not null, telefone int);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
