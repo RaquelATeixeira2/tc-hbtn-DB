937c50c0ae07935ce6dce4e15d087349db604cf8
Raquel Teixeira <raquelat@ciandt.com>
2022-06-27 20:08:46 -0300

    db

---
+++ 0x02/java_jdbc_manipulation/sql_schema_sqlite_database_marco_2022.sql
@@ -0,0 +1,9 @@
+-- TABLE
+CREATE TABLE cliente (id integer primary key, nome varchar(40) not null, idade integer, cpf varchar(40) not null, rg varchar(40));
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
