876506b972338f52459efdd17bd3bc9f05df072e
Raquel Teixeira <raquelat@ciandt.com>
2022-06-27 19:22:05 -0300

    db

---
+++ 0x02/java_jdbc_manipulation/src/ClienteDAOImpl.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;
+
+public class ClienteDAOImpl implements ClienteDAO{
+
+    @Override
+    public Connection connect(String urlConexao) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void createTable(String urlConexao) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void insert(String url_conexao, Cliente cliente) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void selectAll(String urlConexao) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(String urlConexao, int id, String name, Integer idade) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(String urlConexao, int id) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
